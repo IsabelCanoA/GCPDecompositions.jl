85df4b9ed84c984e6d534a8fac9eef67929e806d
Isabel Cano <isabelc@udel.edu>
2025-10-28 11:33:21 -0400

    formatting the code

--- src/api-gmlm.jl
+++ src/api-gmlm.jl
@@ -1,20 +1,20 @@
 using GCPDecompositions, LinearAlgebra
 using GCPDecompositions.TensorKernels
 
-function gmlm(X, Y, r; loss=GCPLosses.LeastSquares())
-	# Extract dimensions
-	n = only(unique([length(X), length(Y)]))
-	M = only(unique(size.(Y)))
-	N = only(unique(size.(X)))
-	P, Q = length(M), length(N)
+function gmlm(X, Y, r; loss = GCPLosses.LeastSquares())
+    # Extract dimensions
+    n = only(unique([length(X), length(Y)]))
+    M = only(unique(size.(Y)))
+    N = only(unique(size.(X)))
+    P, Q = length(M), length(N)
 
-	# Initialization
-	B0 = CPD(ones(r), rand.((N..., M...), r))
-	vu0 = vcat(vec.(B0.U)...)
+    # Initialization
+    B0 = CPD(ones(r), rand.((N..., M...), r))
+    vu0 = vcat(vec.(B0.U)...)
 
     # Setup vectorized objective function and gradient
     vec_cutoffs = (0, cumsum(r .* (N..., M...))...)
-    vec_ranges = ntuple(k -> vec_cutoffs[k]+1:vec_cutoffs[k+1], Val(P+Q))
+    vec_ranges = ntuple(k -> vec_cutoffs[k]+1:vec_cutoffs[k+1], Val(P + Q))
     function f(vu)
         VU = map(range -> reshape(view(vu, range), :, r), vec_ranges)
         return gmlm_objective(CPD(ones(r), VU), X, Y, loss)
@@ -26,12 +26,12 @@ function gmlm(X, Y, r; loss=GCPLosses.LeastSquares())
         return gvu
     end
 
-	# Run LBFGSB
-	algorithm = GCPAlgorithms.LBFGSB(iprint=0)
-	lbfgsopts = (; (pn => getproperty(algorithm, pn) for pn in propertynames(algorithm))...)
+    # Run LBFGSB
+    algorithm = GCPAlgorithms.LBFGSB(; iprint = 0)
+    lbfgsopts = (; (pn => getproperty(algorithm, pn) for pn in propertynames(algorithm))...)
     vu = GCPDecompositions.GCPAlgorithms.lbfgsb(f, g!, vu0; lbfgsopts...)[2]
     VU = map(range -> reshape(vu[range], :, r), vec_ranges)
-	return CPD(ones(r), VU)
+    return CPD(ones(r), VU)
 end
 
 function contract!(result, Xi, B::Array)
@@ -40,7 +40,7 @@ function contract!(result, Xi, B::Array)
     @assert size(result) == m_dims "Result array must have shape $(m_dims)"
 
     Xi_vec = reshape(Xi, 1, :)
-    B_mat  = reshape(B, size(Xi_vec, 2), :)
+    B_mat = reshape(B, size(Xi_vec, 2), :)
     result_vec = reshape(result, 1, :)
 
     mul!(result_vec, Xi_vec, B_mat)
@@ -49,16 +49,16 @@ function contract!(result, Xi, B::Array)
 end
 
 function contract!(result, Xi, B::CPD)
-	temporal_B = Array(B)
-    contract!(result, Xi, temporal_B)
+    temporal_B = Array(B)
+    return contract!(result, Xi, temporal_B)
 end
 
 function gmlm_objective(B, X, Y, loss)
-	n = only(unique([length(X), length(Y)]))
-	M = only(unique(size.(Y)))
-	η = zeros(M)  
+    n = only(unique([length(X), length(Y)]))
+    M = only(unique(size.(Y)))
+    η = zeros(M)
 
-	total = 0.0
+    total = 0.0
     for i in 1:n
         contract!(η, X[i], B)
         @inbounds for j in CartesianIndices(M)
@@ -69,16 +69,16 @@ function gmlm_objective(B, X, Y, loss)
 end
 
 function gmlm_grad!(GVU, B, X, Y, loss)
-	n = only(unique([length(X), length(Y)]))
-	M = only(unique(size.(Y)))
-	N = only(unique(size.(X)))
-	P, Q = length(M), length(N)
+    n = only(unique([length(X), length(Y)]))
+    M = only(unique(size.(Y)))
+    N = only(unique(size.(X)))
+    P, Q = length(M), length(N)
 
-	V, U = collect.(B.U[1:Q]), collect.(B.U[Q+1:end])
-	GV, GU = GVU[1:Q], GVU[Q+1:end]
+    V, U = collect.(B.U[1:Q]), collect.(B.U[Q+1:end])
+    GV, GU = GVU[1:Q], GVU[Q+1:end]
 
-	η  = zeros(M)    
-    Gi = zeros(M)    
+    η = zeros(M)
+    Gi = zeros(M)
 
     _GU = [zero(GU[k]) for k in 1:P]
     _GV = [zero(GV[k]) for k in 1:Q]
